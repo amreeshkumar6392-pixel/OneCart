393adfee01879f3a322b6a5b90f98f4e73b5c928
Amreesh Kumar <amreeshkumar6392@gmail.com>
2026-09-12 18:00:24 +0530

    Fix Google login typo

--- OneCart/backend/controller/authController.js
+++ OneCart/backend/controller/authController.js
@@ -100,7 +100,7 @@ try {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: ture,
+      secure: true,
       sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
