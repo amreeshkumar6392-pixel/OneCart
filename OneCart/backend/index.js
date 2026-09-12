f40525bfb22e22a0bf3e8c04aa339e1466e649ea
amreeshkumar6392-pixel <amreeshkumar6392@gmail.com>
2026-09-12 17:46:26 +0530

    Update index.js

--- OneCart/backend/index.js
+++ OneCart/backend/index.js
@@ -15,7 +15,7 @@ const app = express()
 app.use(cookieParser())
 app.use(express.json())
 app.use(cors({
-  origin:["https://onecart-frontend-rd78.onrender.com","http://localhost:5174"],
+  origin:["https://onecart-frontend-rd78.onrender.com","https://onecart-admin-4u3l.onrender.com"],
   credentials:true
 }))
 
